7728afe6302ca751418e0fe210b9fb938327e89e
Nagaraj_Vh <vhnagarajrakesh@gmail.com>
2025-08-29 13:13:10 +0530

    practicesd from tricky js question

---
+++ Practice/script.js
@@ -0,0 +1,29 @@
+console.log("0" == false); // true
+console.log("0" === false); // false
+
+console.log([null] == 0); // true
+console.log([undefined] == 0); // true
+
+console.log([undefined].toString()); // empty
+console.log([null].toString()); // empty
+console.log([0].toString()); //
+
+console.log([1, 2] == "1,2"); // true
+console.log([] == ![]); // true
+console.log([[]] == ""); // true
+console.log([,] == 0); // true
+
+console.log(null == false); // false
+console.log(null + 1); // 1
+console.log(undefined + 1); // NaN
+console.log(undefined == false); // false
+
+console.log(null < 1); // true
+console.log(null > -1); // true
+console.log(undefined < 1); // false
+console.log(undefined > -1); // false
+
+console.log("0" == false); // true
+console.log("0" == true); // false
+console.log("" == false); // true
+console.log(" " == false); // true
