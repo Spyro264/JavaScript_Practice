aa868ce8ac04b5022733e23d9588ede0d07fe8a1
Nagaraj_Vh <vhnagarajrakesh@gmail.com>
2025-09-18 19:39:41 +0530

    solved leetcode question

--- Array/script.js
+++ Array/script.js
@@ -1,18 +1,3 @@
-const nums = [1, 1, 2, 2, 3, 4, 5, 4];
+const str = "nagaraj";
 
-var removeDuplicates = function (nums) {
-  if (nums.length === 0) return 0;
-
-  let k = 0; // pointer for unique position
-
-  for (let i = 1; i < nums.length; i++) {
-    if (nums[i] !== nums[k]) {
-      k++;
-      nums[k] = nums[i]; // overwrite in place
-    }
-  }
-
-  return k + 1; // number of unique elements
-};
-
-console.log(removeDuplicates(nums));
+console.log(str.split("").reverse().join(""));
